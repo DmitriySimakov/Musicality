37d826b70bf416f6357179903fa8798e8263bbbf
Dmitry Simakov <VAGNER2909@gmail.com>
2020-06-04 11:18:20 +0300

    Add ProgressButton

--- app/src/main/java/com/dmitrysimakov/musicality/ui/AddSongFragment.kt
+++ app/src/main/java/com/dmitrysimakov/musicality/ui/AddSongFragment.kt
@@ -2,18 +2,29 @@ package com.dmitrysimakov.musicality.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import com.dmitrysimakov.musicality.R
 import com.dmitrysimakov.musicality.databinding.FragmentAddSongBinding
 import com.dmitrysimakov.musicality.util.getFileName
 import com.dmitrysimakov.musicality.util.popBackStack
+import com.github.razir.progressbutton.attachTextChangeAnimator
+import com.github.razir.progressbutton.bindProgressButton
+import com.github.razir.progressbutton.showDrawable
+import com.github.razir.progressbutton.showProgress
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 private const val RC_CHOOSE_SONG = 1
 
@@ -41,14 +52,34 @@ class AddSongFragment : Fragment() {
         }
 
         binding.chooseBtn.setOnClickListener { openAudioFiles() }
-        binding.uploadBtn.setOnClickListener { vm.uploadSong() }
+        binding.uploadBtn.apply {
+            attachTextChangeAnimator()
+            bindProgressButton(binding.uploadBtn)
+            setOnClickListener { vm.uploadSong() }
+        }
 
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        vm.uploadedEvent.observe(viewLifecycleOwner) { popBackStack() }
+        vm.uploading.observe(viewLifecycleOwner) {
+            if (it) binding.uploadBtn.showProgress {
+                buttonTextRes = R.string.uploading
+                progressColor = Color.WHITE
+            }
+        }
+        vm.uploadedEvent.observe(viewLifecycleOwner) {
+            CoroutineScope(Dispatchers.Main).launch {
+                val check = ContextCompat.getDrawable(requireContext(), R.drawable.animated_check)!!
+                check.setBounds(0, 0, 40, 40)
+                binding.uploadBtn.showDrawable(check) {
+                    buttonTextRes = R.string.uploaded
+                }
+                delay(1000)
+                popBackStack()
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
